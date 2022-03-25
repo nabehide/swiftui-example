876cf35557edfefd4508d0fb9a4b84ff099b7cf7
nabehide <iasoyv@gmail.com>
2022-03-25 21:58:07 +0900

    add: landmark model

---
+++ Shared/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Hidetoshi Watanabe on 2022/03/25.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
