98c095758e29d08a306f7580821775760a88bc10
nabehide <iasoyv@gmail.com>
2022-03-25 21:19:13 +0900

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Hidetoshi Watanabe on 2022/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
