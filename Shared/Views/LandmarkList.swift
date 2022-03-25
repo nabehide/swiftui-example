ce399035b1e22df43736bc31e8e4df18fd10417a
nabehide <iasoyv@gmail.com>
2022-03-25 22:03:18 +0900

    add: landmark list

---
+++ Shared/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Hidetoshi Watanabe on 2022/03/25.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
