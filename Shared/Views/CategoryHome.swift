27080ace36296478a32140990fd2a983cf780f78
nabehide <iasoyv@gmail.com>
2022-03-28 22:46:18 +0900

    add: category view

---
+++ Shared/Views/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks (iOS)
+//
+//  Created by Hidetoshi Watanabe on 2022/03/28.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
